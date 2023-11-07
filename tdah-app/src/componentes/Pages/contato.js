ad8960fc24b4f477670c640014f8c5878d45e685
matlucaas <rm99505@fiap.com.br>
2023-11-07 01:05:07 -0300

    useForm e alterações na página

--- tdah-app/src/componentes/Pages/contato.js
+++ tdah-app/src/componentes/Pages/contato.js
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import AboutImage from '../Images/image3';
 
 function Contato() {
+
+    const [successMsg, setSuccessMsg] = useState("");
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset
+    } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+        setSuccessMsg("Obrigado pelo feedback!");
+        reset();
+    };
 
     const navigate = useNavigate();
 
@@ -18,11 +33,10 @@ function Contato() {
   }, [navigate]);
 
     const SecAbout = styled.section`
-        width:65%;
+        width:80%;
         margin:0 auto;
         height: auto;
         display: flex;
-        margin-right: 15vw;
     `
     const DivTextAbout = styled.div`
         width:40%;
@@ -53,6 +67,7 @@ function Contato() {
     `
 
     const SecContato = styled.section`
+        padding-top:20px;
         width:85%;
         margin:0 auto;
         height: auto;
@@ -64,30 +79,61 @@ function Contato() {
             <SecAbout>
                 <DivTextAbout>
                     <Title>Quem Somos</Title>
-                    <p>
+                    <p className='textoJustificado'>
                     O TDAH-Connect é um projeto realizado pelos alunos da FIAP em conjunto à IBM, voltado ao tema de cidades sustentáveis. Criamos este projeto para assegurar uma melhora na qualidade de vida de todos os usuários em quesitos de saúde e conhecimento. Nosso site não é restrito apenas aos cidadãos com o transtorno, ele também é destinado a outros públicos que buscam se informar e aprimorar seu intelecto sobre o assunto. Com isso, esperamos que com todas as ferramentas contidas, as dificuldades diárias durante a execução de tarefas sejam reduzidas e suas performances melhoradas. 
                         
                     </p>
                     <br/>
-                    <p>
+                    <p className='textoJustificado'>
                         O intuito prioritário do projeto é que sirva de pilar ao aprendizado e conhecimento, fluindo-se totalmente contra os preconceitos e estigmas remanescentes, que ainda atingem e assolam uma parte muito considerável da população brasileira. Quanto maior visibilidade sobre nossas iniciativas, maior o número de tratamentos precoces e cada vez menos impasses sociais negativos.​
                     </p>
                 </DivTextAbout>
                 <div>
-                    <AboutImage/>
+                    <AboutImage className='imgContato'/>
                 </div>
             </SecAbout>
 
             <DivSpace/><DivSpace/>
 
             <TitleC>Contate-Nos</TitleC>
+
+            <div className="containerContateNos">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form-control">
+                    <label>Email</label>
+                    <input
+                        type="text"
+                        {...register("username", {
+                        required: "Insira seu nome."
+                        })}
+                    />
+                    {errors.email && <p className="errorMsg">{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control">
+                        <label>Feedback</label>
+                        <input
+                        type="text"
+                        {...register("username", {
+                        required: "Insira o feedback."
+                        })}
+                        />
+                        {errors.email && <p className="errorMsg">{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control">
+                        <label></label>
+                        <button type="submit">Enviar</button>
+                    </div>
+                    {successMsg && <p className="success-msg">{successMsg}</p>}
+                </form>
+            </div>
+
             <SecContato>
             <h3> Desenvolvedores:</h3>
-            <h4> Bruno Silva Bastos  -- brunosbastos5@gmail.com</h4>
-            <h4> Gabriel Freitas -- gabrielmaradei2017@gmail.com</h4>
-            <h4> Guilherme Daher -- guidaher2004@gmail.com</h4>
-            <h4> Matheus Lucas -- matheus.lob1379@gmail.com</h4>
-            <h4> Número para contato -  55 1198952-5531</h4>
+            <p> Bruno Silva Bastos  -- brunosbastos5@gmail.com</p>
+            <p> Gabriel Freitas -- gabrielmaradei2017@gmail.com</p>
+            <p> Guilherme Daher -- guidaher2004@gmail.com</p>
+            <p> Matheus Lucas -- matheus.lob1379@gmail.com</p>
+            <p> Número para contato -  55 1198952-5531</p>
             </SecContato>
         </div>
     );
